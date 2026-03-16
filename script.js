1f330181e829173082eaef0d2c6abaeca227b085
ParkWash <92950458+nexsocom@users.noreply.github.com>
2026-03-17 00:22:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,11 +4,14 @@ window.addEventListener("scroll",()=>{
 
 const heroContent = document.querySelector(".heroContent")
 
+if(heroContent){
+
 let scroll = window.scrollY
 
 heroContent.style.transform = `translateY(${scroll*0.25}px)`
-
 heroContent.style.opacity = 1 - scroll/500
+
+}
 
 })
 
@@ -20,18 +23,19 @@ heroContent.style.opacity = 1 - scroll/500
 document.querySelectorAll("video").forEach(video=>{
 
 video.addEventListener("mouseenter",()=>{
-
 video.play()
-
 })
 
 video.addEventListener("mouseleave",()=>{
-
 video.pause()
-
 })
 
 })
+
+
+
+
+// SERVICE PAGE PARALLAX
 
 const heroImage = document.querySelector(".serviceHero img");
 
@@ -44,44 +48,5 @@ const scroll = window.scrollY;
 heroImage.style.transform = "translateY(" + scroll * 0.35 + "px)";
 
 }
-
-});
-
-window.addEventListener("load", () => {
-
-document.body.classList.add("loaded");
-
-});
-
-// PAGE FADE IN
-window.addEventListener("load", () => {
-  document.body.classList.add("loaded");
-});
-
-
-// PAGE FADE OUT (alleen voor echte pagina links)
-document.querySelectorAll("a").forEach(link => {
-
-  const href = link.getAttribute("href");
-
-  if(!href) return;
-
-  if(href.startsWith("#")) return; // skip anchors
-
-  if(href.startsWith("http")) return; // skip externe links
-
-  link.addEventListener("click", function(e){
-
-    e.preventDefault();
-
-    document.body.classList.remove("loaded");
-
-    setTimeout(()=>{
-
-      window.location.href = href;
-
-    }, 500);
-
-  });
 
 });
